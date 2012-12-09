383eb6c9b29533e417464ee90c4077b4fa621c03
David Robbins <dave@wakanda.org>
2012-12-09 10:05:13 -0800

    added the new login component and read me.

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-betterWakandaLoginComponent
-===========================
+# Wakanda Login Component
 
-Custom Login Interface for your Wakanda Application
+This Wakanda Component Set provides a custom interface for authenticating users in your Wakanda application.
+
+## Quickstart
+
+* Download Wakanda from http://wakanda.org/downloads/
+
+* Launch Wakanda Studio and open your existing project or create a new one.
+
+* Place the "login.waComponent" folder at the root level of the WebFolder.
+
+* On your interface page drag a component widget onto your page. From the Styles section of the Properties Area make the width 625 and the height 86.
+
+* That's all folks. 
+
+
+## Features
+Provides a custom login and logout interface for authenticating users in your Wakanda application.
+
+
+## Feedback
+
+This is my second attempt at creating a reusable component with Wakanda. If you have any questions feel free to drop me a line at dave@wakanda.org.
+
+## License
+
+Release under the MIT license.
+
+December 4, 2012.
---
+++ login.waComponent/login.js
@@ -0,0 +1,100 @@
+﻿
+(function Component (id) {// @lock
+
+// Add the code that needs to be shared between components here
+
+function constructor (id) {
+	var loginNameField = getHtmlId('loginNameTextField'),
+	loginPasswordField = getHtmlId('loginPasswordTextField'),
+	errorMsg = getHtmlId('errorMessageRichText'),
+	loginContainerRef = getHtmlId('loginContainer'),
+	logoutContainerRef = getHtmlId('logoutContainer'),
+	statusMsg = getHtmlId('signInStatusMessage');
+	
+	// @region beginComponentDeclaration// @startlock
+	var $comp = this;
+	this.name = 'login';
+	// @endregion// @endlock
+
+	function toggleLogin() {
+		if (waf.widgets[loginContainerRef].isVisible()) {
+			$("#" + loginContainerRef).fadeOut(400); //hide() 
+			$("#" + logoutContainerRef).fadeIn(900); //show() 
+		} else {
+			$("#" + loginContainerRef).fadeIn(900, function() {$$(loginNameField).focus();});; //show
+			$("#" + logoutContainerRef).fadeOut(400); //hide 
+		}
+	}
+	
+	
+	function signIn() {
+		if (waf.directory.loginByPassword($("#" + loginNameField).val(), $("#" + loginPasswordField).val())) {
+			//Our user signed in successfully.
+			toggleLogin();
+			$$(statusMsg).setValue("Signed in as: " + waf.directory.currentUser().fullName);
+			$$(loginNameField).setValue("");
+			$$(loginPasswordField).setValue("");
+			$$(errorMsg).setValue("");
+			$$(loginNameField).focus();
+			
+		} else {
+			$$(errorMsg).setValue("Invalid login.");
+			$$(statusMsg).setValue("");
+		}
+	}
+	
+	
+	this.load = function (data) {// @lock
+		//Move our logout section into place.
+		$("#" + logoutContainerRef).css("top", "0px");
+		$("#" + logoutContainerRef).css("left", "0px");			
+			
+		//If we have a current user signed in then show login container, else show logout container.
+		if (WAF.directory.currentUser() === null) {
+			//No user is signed in.
+			$("#" + loginContainerRef).show(200, function() {$$(loginNameField).focus();}); //show
+			$("#" + logoutContainerRef).hide(); //hide
+			
+		} else {
+			//We have a user signed in.
+			$("#" + loginContainerRef).hide(); //show
+			$("#" + logoutContainerRef).show(); //hide
+			$$(statusMsg).setValue("Signed in as: " + waf.directory.currentUser().fullName); 	
+		}
+		
+		//Make return key trigger login when user name or password input fields have focus.
+		$("#" + loginNameField + ", #" + loginPasswordField).live('keyup', function (e) {
+	   		if ( e.keyCode == 13 ){
+	   			signIn();
+	    	}
+		});
+	// @region namespaceDeclaration// @startlock
+	var loginButton = {};	// @button
+	var logoutButton = {};	// @button
+	// @endregion// @endlock
+
+	// eventHandlers// @lock
+
+	loginButton.click = function loginButton_click (event)// @startlock
+	{// @endlock
+		signIn();
+	};// @lock
+
+	logoutButton.click = function logoutButton_click (event)// @startlock
+	{// @endlock
+		if (WAF.directory.logout()) {
+			toggleLogin();
+		}
+	};// @lock
+
+	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_loginButton", "click", loginButton.click, "WAF");
+	WAF.addListener(this.id + "_logoutButton", "click", logoutButton.click, "WAF");
+	// @endregion// @endlock
+
+	};// @lock
+
+
+}// @startlock
+return constructor;
+})();// @endlock
